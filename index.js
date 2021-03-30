7507476cf640a7ee28c24b08549a840d643e7cd2
jahidhasan6484 <jahidhasan6484@gmail.com>
2021-03-30 23:38:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const admin = require('firebase-admin');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j3ujg.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+
+const port = 5000
+
+const app = express()
+app.use(cors());
+app.use(bodyParser.json());
+
+var serviceAccount = require("./configs/burj-al-arab-39af4-firebase-adminsdk-h0stl-5aa6f0ea14.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
+const pass = "ArabianHorse79";
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const bookings = client.db("burjAlArab").collection("bookings");
+
+    app.post('/addBooking', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/bookings', (req, res) => {
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            console.log({ idToken });
+
+            // idToken comes from the client app
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    if (tokenEmail == queryEmail) {
+                        bookings.find({ email: queryEmail })
+                            .toArray((err, documents) => {
+                                res.status(200).send(documents);
+                            })
+                    }
+                    else {
+                        res.status(401).send("unauthorized access");
+                    }
+                })
+                .catch((error) => {
+                    res.status(401).send("unauthorized access");
+                });
+        }
+        else {
+            res.status(401).send("unauthorized access");
+        }
+    })
+});
+
+app.listen(port)
